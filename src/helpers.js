663004c79fe915f435ebb4ab21bf8efd0ae2d8ed
adorocode <rychell@maquiadoro.com.br>
2021-01-02 20:20:30 -0300

    Some new features added

--- src/helpers.js
+++ src/helpers.js
@@ -8,7 +8,14 @@ const getValuesToAnOption = (obj, path) => {
 
     let currentLevelData = obj
     for (let i = 0; i < arrPath.length; i++) {
-        currentLevelData = currentLevelData.map(data => data[arrPath[i]])
+        currentLevelData = currentLevelData.flatMap(data => {
+            if (Array.isArray(data)){
+                return data.map(d=>_.get(d, arrPath[i]))
+            }
+            else{
+                return _.get(data, arrPath[i])
+            }
+        })
     }
     return currentLevelData
 }
@@ -17,17 +24,22 @@ const getValueOfObject = (obj, path) => {
 
     const arrPath = path.split('.')
     for (let i = 0; i < arrPath.length; i++) {
-        obj = obj[arrPath[i]];
+        if (Array.isArray(obj)){
+            obj = obj.map(data => _.get(data, arrPath[i]))
+        }
+        else{
+            obj = _.get(obj, arrPath[i]);
+        }
     }
     return obj
 }
 
-const setValueToObject = (obj, path, data, alias) => {
+const setValueToObject = (obj, path, data, alias, keepOriginalKey = false) => {
     // TODO: validate arguments
     arrNodes = []
     arrNodes[0] = _.cloneDeep(obj)
     arrPath = path.split('.')
-
+    
     for (let i = 0; i < arrPath.length; i++) {
         const node = arrNodes[i][arrPath[i]];
         if (i + 1 === arrPath.length) {
@@ -40,7 +52,8 @@ const setValueToObject = (obj, path, data, alias) => {
         const path = arrPath[i - 1];
         if (alias && i == arrPath.length) {
             arrNodes[i - 1][alias] = arrNodes[i]
-            delete arrNodes[i - 1][path]
+            if(!keepOriginalKey){delete arrNodes[i - 1][path]
+            }    
             break
         }
         arrNodes[i - 1][path] = arrNodes[i]
--- src/index.js
+++ src/index.js
@@ -9,24 +9,29 @@ const KnexPopulatePlugin = function (knexInstance) {
   this.knex = knexInstance
   this.dataToPopulate = []
   this.populatedData = []
+  this.verbose = false
 }
 
 KnexPopulatePlugin.prototype.loadData = function (dataToPopulate) {
   this.dataToPopulate = dataToPopulate
   return this
 }
+KnexPopulatePlugin.prototype.verbose = function (verbose) {
+  this.verbose = verbose
+  return this
+}
 KnexPopulatePlugin.prototype.populate = function (options) {
   this.options = options
   return this
 }
-
 KnexPopulatePlugin.prototype.exec = async function () {
   let dataToPopulate = this.dataToPopulate
+  const verbose = this.verbose
   // const options = this.options
   const knex = this.knex
   const dataset = {}
   const populatedData = this.populatedData
-  
+
   // TODO: Here validate all stuff necessary and put propper error messages
 
   // First step - Mount queries to search data on db based on options array
@@ -35,29 +40,62 @@ KnexPopulatePlugin.prototype.exec = async function () {
     const options = this.options.filter(option => option.key.split('.').length - 1 === k)
     for (let i = 0; i < options.length; i++) {
       const option = options[i];
-      const arrOptionValues = getValuesToAnOption(dataToPopulate, option.key)
+      const filters = option.filters || {}
+      const path = option.key
+      const uniqueKey = `${option.alias || ''}${option.key}`
+      const arrOptionValues = getValuesToAnOption(dataToPopulate, path)
       const uniqueOptionValues = [...new Set(arrOptionValues)]
-      if (uniqueOptionValues.length){
-        dataset[option.key] = await knex(option.searchOnTable).select(option.searchColumns || '*').whereIn(option.matchingColumn || 'id', uniqueOptionValues)
-        if(option.onFind){
-          dataset[option.key] = option.onFind(dataset[option.key])
+      if (uniqueOptionValues
+         && uniqueOptionValues.some(option => option)
+         && uniqueOptionValues.length) {
+        const query = knex(option.searchOnTable).select(option.searchColumns || '*').whereIn(option.matchingColumn || 'id', uniqueOptionValues).where(filters)
+        dataset[uniqueKey] = await query
+        if (verbose) console.log(await query.toSQL().toNative());
+        if (option.onFind) {
+          dataset[uniqueKey] = option.onFind(dataset[uniqueKey])
         }
+      }
+      else{
+        dataset[uniqueKey] = undefined
       }
     }
 
     for (let i = 0; i < dataToPopulate.length; i++) {
       for (let j = 0; j < options.length; j++) {
+        let resultadoParaPopular = []
         const option = options[j];
         const path = option.key
-        const alias = option.alias
-        const currentValueOnKey = getValueOfObject(dataToPopulate[i], option.key)
-        const resultadoParaPopular = dataset[path].find(data => data[option.matchingColumn || 'id'] === currentValueOnKey)
-        populatedData[i] = setValueToObject(dataToPopulate[i], path, resultadoParaPopular, alias)
-        dataToPopulate [i]= _.cloneDeep(populatedData[i])  
+        const {
+          alias,
+          multiple,
+          keepOriginalKey
+        } = option
+        const uniqueKey = `${option.alias || ''}${option.key}`
+
+        const currentValueOnKey = getValueOfObject(dataToPopulate[i], path)
+        const searchMethod = multiple ? 'filter' : 'find'
+        
+        if(dataset[uniqueKey]) {
+          if (Array.isArray(currentValueOnKey)) {
+            for (let index = 0; index < currentValueOnKey.length; index++) {
+              const valueOnKey = currentValueOnKey[index];
+              let arrPath = path.split('.')
+              pathLength = arrPath.length
+              arrPath[pathLength] = arrPath[pathLength - 1]
+              arrPath[pathLength - 1] = index
+              resultadoParaPopular = dataset[uniqueKey][searchMethod](data => data[option.matchingColumn || 'id'] === valueOnKey)
+              populatedData[i] = setValueToObject(dataToPopulate[i], arrPath.join('.'), resultadoParaPopular, alias, keepOriginalKey)
+              dataToPopulate[i] = _.cloneDeep(populatedData[i])
+            }
+          } else {
+            resultadoParaPopular = dataset[uniqueKey][searchMethod](data => data[option.matchingColumn || 'id'] === currentValueOnKey)
+            populatedData[i] = setValueToObject(dataToPopulate[i], path, resultadoParaPopular, alias, keepOriginalKey)
+          }
+          dataToPopulate[i] = _.cloneDeep(populatedData[i])
+        }
       }
     }
     dataToPopulate = _.cloneDeep(populatedData)
-    
   }
   this.dataToPopulate = []
   this.populatedData = []
